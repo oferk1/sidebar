6a3907320975e5fa9de2d048d5a28daf6da085cb
ofer <ofer.kafri@zooz.com>
2017-04-07 10:37:34 +0300

    port change

--- routes/sidebar.js
+++ routes/sidebar.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+	res.set({"Access-Control-Allow-Origin": "*", "Content-Type": "application/json"});
   res.json(sidebar);
 });
 
