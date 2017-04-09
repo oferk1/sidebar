91726b19fc67ad8a67491a4ab1c9ebb89a1eba18
ofer <ofer.kafri@zooz.com>
2017-04-09 15:30:55 +0300

    add cors

--- routes/sidebar.js
+++ routes/sidebar.js
@@ -5,7 +5,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	res.set({"Access-Control-Allow-Origin": "*", "Content-Type": "application/json"});
-  res.json(sidebar);
+	res.set({"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE", "Content-Type": "application/json"});
+	res.json(sidebar);
 });
 
 module.exports = router;
